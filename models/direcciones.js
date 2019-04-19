b37086ad4638bd8a289c2ec3df78aeecc700c38d
Salma Lizeth Mazuca Reyna <salmalizeth18_MR@hotmail.com>
2019-04-18 23:06:08 -0500

    autores

---
+++ models/direcciones.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var sql = require('./db.js')
+
+var Direcciones = function(direccion){
+    this.ID = direccion.ID;
+    this.Calle = direccion.Calle;
+    this.Numero = direccion.Numero;
+    this.Colonia = direccion.Colonia;
+    this.Ciudad = direccion.Ciudad;
+    this.Pais = direccion.Pais;
+    this.
+}
