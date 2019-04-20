e258635d4da948cb916276936fc8803e3e199bec
1Postage <basictricks94@gmail.com>
2019-04-19 21:02:01 -0500

    renombrar columnas de la bd

--- models/libro.js
+++ models/libro.js
@@ -2,32 +2,21 @@
 
 var sql = require('./db.js')
 
-const selectors = 'lib.ID AS lib_id, lib.titulo, lib.isbn,lib.paginas, lib.descripcion_fisica, ' +
-'lib.descripcion, gen.gen_nombre AS genero,  aut.nombre AS autor, img.data AS imgdata, img.img_filename AS filename,  ' + 
-'aut.ID as autor_id, gen.ID as gen_id ';
+const selectors = 'lib.lib_id, lib.titulo, lib.isbn,lib.paginas, lib.descripcion_fisica, ' +
+'lib.descripcion, gen.gen_nombre AS genero, aut.aut_nombre AS autor, img.data AS imgdata,'+
+' img.img_filename AS filename,  ' + 
+'aut.aut_id AS autor_id, gen.gen_id AS gen_id ';
 
-const genero = ' FROM libros AS lib INNER JOIN generos AS gen ON (lib.genero_id = gen.ID) ' +
-    ' INNER JOIN autor_libro AS al ON (al.libro_id = lib.ID) ' +
-    ' INNER JOIN autores AS aut ON (aut.ID = al.autor_id) ';
+const genero = ' FROM libros AS lib'+
+    ' INNER JOIN generos AS gen ON (lib.genero_id = gen.gen_id) ' +
+    ' INNER JOIN autor_libro AS al ON (al.libro_id = lib.lib_id) ' +
+    ' INNER JOIN autores AS aut ON (aut.aut_id = al.autor_id) ';
 
-const imageJOIN = ' LEFT JOIN imagen_libro AS img ON (lib.ID = img.libro_id) '
+const imageJOIN = ' LEFT JOIN imagen_libro AS img ON (lib.lib_id = img.libro_id) '
 
 
 const insert = "INSERT INTO libros(titulo, orig_titulo, isbn, paginas, descripcion_fisica, descripcion, editorial_id, genero_id)" +
     "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-
-// Constructor de Libro
-// var Libro = (libro) => {
-//     this.ID = libro.ID
-//     this.titulo = libro.titulo
-//     this.tituloOrig = libro.tituloOrig
-//     this.isbn = libro.isbn
-//     this.paginas = libro.paginas
-//     this.descripcion = libro.descripcion
-//     this.descripcion_fisica = libro.descripcion_fisica
-//     this.editorial_id = libro.editorial_id
-//     this.genero_id = libro.genero_id
-// }
 
 class Libro {
     constructor(titulo, tituloO, isbn, paginas, descripcion, descripcion_fisica, gen_id, ed_id) {
@@ -62,7 +51,7 @@ Libro.createLibro = function createUser(newLibro, result) {
 
 Libro.getLibroById = function createUser(libroId, result) {
     // '?' es un placeholder como %s o %d
-    sql.query("SELECT " + selectors + genero + imageJOIN + " WHERE lib.id = ? ", libroId, function (err, res) {
+    sql.query("SELECT " + selectors + genero + imageJOIN + " WHERE lib.lib_id = ? ", libroId, function (err, res) {
         if (err) {
             console.log("error :", err)
             result(err, null)
@@ -73,8 +62,7 @@ Libro.getLibroById = function createUser(libroId, result) {
 }
 
 Libro.getAllLibros = function getAllLibros(result) {
-    sql.query("SELECT * FROM libros " +
-        "AS lib INNER JOIN generos AS gen ON (lib.genero_id = gen.id) ", function (err, res) {
+    sql.query("SELECT " + selectors + genero + imageJOIN , function (err, res) {
             if (err) {
                 console.log("error: ", err)
                 result(null, err)
