89deceed73cc8d7c7d8de0aa73d2afb8dc6e992e
1Postage <basictricks94@gmail.com>
2019-04-24 18:46:53 -0500

    busqueda de generos

--- controllers/BusquedaController.js
+++ controllers/BusquedaController.js
@@ -5,10 +5,29 @@ var Editorial = require('../models/editorial');
 var Genero = require('../models/genero');
 var Autor = require('../models/autor');
 
-exports.find = (req, res) => {
+exports.find = async (req, res) => {
     var search = req.body.search;
+    let libros = await getLibros(search);
+    let generos = await getGeneros(search);
+    res.json({libros, generos})
+}
 
+function getGeneros(search) {
+    return new Promise((resolve, reject) => {
+        Genero.findByNombre(search, (err, res) => {
+            if (!err) {
+                resolve(res);
+            }
+        });
+    });
+}
 
-
-    res.json(req.body);
+function getLibros(search) {
+    return new Promise((resolve, reject) => {
+        Libro.find(search, (err, res) => {
+            if (!err) {
+                resolve(res);
+            }
+        });
+    });
 }
