d81a3bf5875ffc121c85f0f6eba468404461ca29
Salma Lizeth Mazuca Reyna <salmalizeth18_MR@hotmail.com>
2019-04-25 13:25:40 -0500

    usuario

--- models/usuario.js
+++ models/usuario.js
@@ -2,7 +2,13 @@
 
 var sql = require('./db.js')
 
-var Usuario = function (nombre, apellidos, email, username, password, fecha_nacimiento) {
+const Insert = 'INSERT into Usuarios(us_nombre, us_apellidos, us_email, us_email, us_username, us_password, us_fecha_nacimiento, VALUES(?,?,?,?,?,?))';
+const update = 'UPDATE Usuarios SET us_nombre = ?, us_email = ?, us_username = ?, us_password = ?, us:fecha_nacimiento = ?' + 
+               'WHERE us_nombr = ?';
+const Delete = 'DELETE from Usuarios WHERE us_nombre = ?';
+
+class usuario {
+    constructor(nombre, apellidos, email, username, password, fecha_nacimiento){
     //this.ID = usr.id,
     this.nombre = nombre
     this.apellidos = apellidos
@@ -11,6 +17,52 @@ var Usuario = function (nombre, apellidos, email, username, password, fecha_naci
     this.password = password
     this.fecha_nacimiento = fecha_nacimiento
     //this.direccion_id = usr.direccion_id,
+  }
+
+
+    save(result){
+      sql.query(Insert, 
+        [this.nombre, this.apellidos,this.email, 
+        this.username, this.password, this.fecha_nacimiento],
+         (err, res) => {
+             if(err){
+                console.log("ERROR:", err);
+                result(err, null);
+            } else {
+           result(null, res);
+       }
+   })
+  }
+
+  update(result){
+    sql.query(update,
+        [this.nombre, this.apellidos,this.email, 
+        this.username, this.password, this.fecha_nacimiento],
+        (err, res) => {
+            if (err) {
+                console.log("error :", err)
+                result(err, null)
+            } else {
+                console.log(res)
+                result(null, res)
+            }
+        });
+}
+
+    delete(result){
+        sql.query(Delete, [this.nombre, this.apellidos,this.email, 
+            this.username, this.password, this.fecha_nacimiento],
+        (err, res) =>{
+            if(err){
+                console.log("Error: ", err)
+                result(err,null)
+            } else {
+                console.log(res)
+                result(null, res)
+            }
+        })
+    }
+
 }
 
 Usuario.crearUsuario = function crearUsuario(usr, result) {
