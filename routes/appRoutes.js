f89d111b494cf377d8e4721a90db60ba768dae62
1Postage <basictricks94@gmail.com>
2019-04-26 18:03:56 -0500

    routa login usuario

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -48,6 +48,7 @@ router.get('/usuarios/', usuario.list_all_users);
 router.post('usuarios/', usuario.create_usr);
 router.get('/usuarios/register', usuario.getRegister);
 router.post('/usuarios/register', usuario.create_usr);
+router.get('/usuarios/login', usuario.formLogin);
 
 router.get('/autores/', autor.list_all_authors);
 router.get('/autores/d/:aut_id', autor.listBooksOf);
