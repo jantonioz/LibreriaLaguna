ace554780659e186133e29d926d2a47dfcac661f
1Postage <basictricks94@gmail.com>
2019-05-14 16:11:40 -0500

    direccion auditable

--- models/direccion.js
+++ models/direccion.js
@@ -4,15 +4,20 @@ var sql = require('./db.js')
 
 const fields = {
     calle: 'dir_calle', numero: 'dir_num', colonia: 'dir_colonia',
-    ciudad: 'dir_cd', pais: 'dir_pais', cp: 'dir_cp'
+    ciudad: 'dir_cd', pais: 'dir_pais', cp: 'dir_cp', ses_id: 'ses_id'
 };
 
 const assign = ' = ?';
 const comma = ', ';
 
-const INSERT = 'INSERT INTO direcciones(' + fields.calle + comma +
-    fields.numero + comma + fields.colonia + comma + fields.ciudad + comma +
-    fields.pais + comma + fields.cp + comma + 'created_at) VALUES (?, ?, ?, ?, ?, ?, NOW())';
+const INSERT = 'INSERT INTO direcciones(' + 
+    fields.calle + comma +
+    fields.numero + comma + 
+    fields.colonia + comma + 
+    fields.ciudad + comma +
+    fields.pais + comma + 
+    fields.cp + comma + 
+    fields.ses_id + comma + 'created_at, updated_at VALUES (?, ?, ?, ?, ?, ?, ?, NOW(), NOW())';
 
 const UPDATE = 'UPDATE direcciones SET ' +
             fields.calle + assign + comma +
@@ -21,10 +26,11 @@ const UPDATE = 'UPDATE direcciones SET ' +
             fields.ciudad + assign + comma +
             fields.pais  + assign + comma +
             fields.cp + assign + 
+            fields.ses_id + assign +
             ' WHERE dir_id' + assign;
 
 class Direccion {
-    constructor(calle, numero, colonia, ciudad, pais, cp, id = 0) {
+    constructor(calle, numero, colonia, ciudad, pais, cp, ses_id, id = 0) {
         this.calle = calle;
         this.numero = numero;
         this.colonia = colonia;
@@ -32,10 +38,11 @@ class Direccion {
         this.pais = pais;
         this.cp = cp;
         this.id = id;
+        this.ses_id = ses_id;
     }
 
     save(result) {
-        sql.query(INSERT, [this.calle, this.numero, this.colonia, this.ciudad, this.pais, this.cp],
+        sql.query(INSERT, [this.calle, this.numero, this.colonia, this.ciudad, this.pais, this.cp, this.ses_id],
             (err, res) => {
                 if (err) {
                     console.log("ERROR: ", err);
@@ -50,7 +57,7 @@ class Direccion {
 
     update() {
         return new Promise((resolve, reject) => {
-            sql.query(UPDATE, [this.calle, this.numero, this.colonia, this.ciudad, this.pais, this.cp, this.id],
+            sql.query(UPDATE, [this.calle, this.numero, this.colonia, this.ciudad, this.pais, this.cp, this.ses_id, this.id],
                 (err, res) => {
                     if (!err) {
                         resolve(res);
