738b9bb7e57b6165afdf66f02b7cb90e232c672d
1Postage <basictricks94@gmail.com>
2019-05-14 15:37:05 -0500

    autor_libros auditable

--- models/autor_libro.js
+++ models/autor_libro.js
@@ -2,21 +2,22 @@
 
 var sql = require('./db.js')
 
-const INSERT = 'INSERT INTO autor_libro(autor_id, libro_id) VALUES(?, ?)';
+const INSERT = 'INSERT INTO autor_libro(autor_id, libro_id, ses_id) VALUES(?, ?, ?)';
 const UPDATE = 'UPDATE autor_libro SET autor_id = ?, libro_id = ? WHERE ' +
     'autor_id = ? AND libro_id = ?';
 
-// Constructor de A U T O R - L I B R O
+
 class AutorLibro {
-    constructor(autor_id, libro_id) {
+    constructor(autor_id, libro_id, ses_id) {
         this.autor_id = autor_id;
         this.libro_id = libro_id;
+        this.ses_id = ses_id;
     }
 
 
     save() {
         return new Promise((resolve, rejec) => {
-            sql.query(INSERT, [this.autor_id, this.libro_id], (err, res) => {
+            sql.query(INSERT, [this.autor_id, this.libro_id, this.ses_id], (err, res) => {
                 if (err) {
                     console.log("ERROR:", err);
                     resolve(-1);
