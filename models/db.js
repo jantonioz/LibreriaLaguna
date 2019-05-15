f9a17b534cd372807b48e4ebc048a313f67fc310
1Postage <basictricks94@gmail.com>
2019-05-14 21:36:24 -0500

    change db to auditable

--- models/db.js
+++ models/db.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'test_libreria',
+    database: 'test_libreria2',
 })
 
 connection.connect((err) => {
